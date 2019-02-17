bc63bf0a43c3f2439599b23c34bb17e0a1bb9771
Sinan Halilbeyoglu <sinan.halilbeyoglu@gmail.com>
2019-02-17 21:00:07 +0000

    Added notes for array imp

--- DequesAndRandomQueues/RandomizedQueue.java
+++ DequesAndRandomQueues/RandomizedQueue.java
@@ -7,6 +7,13 @@
  *
  **************************************************************************** */
 
+// TODO: might be best to use resizing array
+// enqueue -> if queue full && arr[0] == null, shift items to left by copying over to same size array
+// else if full && arr[0] != null, double size of arr and copy over
+
+// dequeue -> shrinking algorithm is trickier, both sides need trimming
+// need to keep track of head and tail index
+// (tail - head) == s.length / 4 => halve array and copy over items
 public class RandomizedQueue {
     public static void main(String[] args) {
 
