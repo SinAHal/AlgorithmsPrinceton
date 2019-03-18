92c6fe3c7c2079b070dba437fa76aaf2119e69c8
Sinan Halilbeyoglu <sinan.halilbeyoglu@gmail.com>
2019-03-18 22:11:41 +0000

    todo: fix ordering of combo

--- collinear/src/main/java/BruteCollinearPoints.java
+++ collinear/src/main/java/BruteCollinearPoints.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.LinkedList;
 
 public class BruteCollinearPoints {
@@ -6,21 +10,22 @@ public class BruteCollinearPoints {
 
     public BruteCollinearPoints(Point[] points) {
         validatePoints(points);
+        segments = new LinkedList<LineSegment>();
         findSegments(points, 4);
     }
 
     private void validatePoints(Point[] points) {
-        if(points == null)
+        if (points == null)
             throw new IllegalArgumentException();
 
-        if(points[points.length-1]==null)
+        if (points[points.length-1] == null)
             throw new IllegalArgumentException();
 
-        for(int i=0; i<points.length-1; i++) {
-            if(points[i]==null)
+        for (int i = 0; i < points.length-1; i++) {
+            if (points[i] == null)
                 throw new IllegalArgumentException();
             // should be no repeated points
-            if(points[i].compareTo(points[i+1]) == 0)
+            if (points[i].compareTo(points[i+1]) == 0)
                 throw new IllegalArgumentException();
         }
     }
@@ -32,28 +37,30 @@ public class BruteCollinearPoints {
 
     // Returns each line segment containing 4 points exactly once
     public LineSegment[] segments() {
-        return (LineSegment[]) segments.toArray();
+        return segments.toArray(new LineSegment[0]);
     }
 
     private boolean isCollinear(Point[] points) {
         double slope = points[0].slopeTo(points[1]);
-        for(int i=1; i<points.length-1; i++) {
-            if(points[i].slopeTo(points[i+1]) != slope)
+        for (int i = 1; i < points.length-1; i++) {
+            if (points[i].slopeTo(points[i+1]) != slope)
                 return false;
         }
         return true;
     }
 
     private void findSegments(Point[] combo, int cdx, int ndx, Point[] elems) {
-        if(ndx == elems.length && cdx == combo.length && isCollinear(combo)) {
+        if (ndx == elems.length && cdx == combo.length && isCollinear(combo)) {
+            // TODO: points might be out-of-order in segment, FIX ORDERING
             LineSegment seg = new LineSegment(combo[0], combo[cdx-1]);
             segments.add(seg);
         }
 
-        if(ndx < elems.length) {
-            if(cdx < combo.length) {
+        if (ndx < elems.length) {
+            if (cdx < combo.length) {
                 combo[cdx] = elems[ndx];
-                if(cdx>0 && combo[cdx-1].compareTo(combo[cdx])>0)
+                // TODO: introduce for efficiency
+                // if (cdx > 0 && combo[cdx-1].compareTo(combo[cdx]) > 0)
                     findSegments(combo, cdx + 1, ndx + 1, elems);
             }
 
@@ -64,5 +71,35 @@ public class BruteCollinearPoints {
     private void findSegments(Point[] points, int r) {
         Point[] combination = new Point[r];
         findSegments(combination, 0, 0, points);
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 }
--- collinear/src/test/java/TestBruteCollinearPoints.java
+++ collinear/src/test/java/TestBruteCollinearPoints.java
@@ -4,14 +4,40 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
 import junit.framework.TestCase;
 
 public class TestBruteCollinearPoints extends TestCase {
 
-    public void testrecursiveCombinations() {
-//        Integer[] ints = {1,2,3,4,5,6};
-//        BruteCollinearPoints brute = new BruteCollinearPoints();
-//        brute.recursiveCombinations(ints, 4);
+    public void testBrute() {
+        // read the n points from a file
+        In in = new In("input8.txt");
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        LineSegment[] segs = collinear.segments();
+        for (LineSegment segment : segs) {
+            System.out.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 
 }
