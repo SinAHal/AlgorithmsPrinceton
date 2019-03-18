c0c4e95e72d75c967a05532b3d434d3ef9733253
Sinan Halilbeyoglu <sinan.halilbeyoglu@episerver.com>
2019-03-18 21:31:37 +0000

    first attempt at brute collinear

--- collinear/src/main/java/BruteCollinearPoints.java
+++ collinear/src/main/java/BruteCollinearPoints.java
@@ -1,38 +1,68 @@
-import java.util.Arrays;
+import java.util.LinkedList;
 
 public class BruteCollinearPoints {
 
-    // finds all line segments containing 4 points
-    public BruteCollinearPoints(Point[] points) {
+    private LinkedList<LineSegment> segments;
 
+    public BruteCollinearPoints(Point[] points) {
+        validatePoints(points);
+        findSegments(points, 4);
+    }
+
+    private void validatePoints(Point[] points) {
+        if(points == null)
+            throw new IllegalArgumentException();
+
+        if(points[points.length-1]==null)
+            throw new IllegalArgumentException();
+
+        for(int i=0; i<points.length-1; i++) {
+            if(points[i]==null)
+                throw new IllegalArgumentException();
+            // should be no repeated points
+            if(points[i].compareTo(points[i+1]) == 0)
+                throw new IllegalArgumentException();
+        }
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return segments().length;
+        return segments.size();
     }
 
-    // the line segments
+    // Returns each line segment containing 4 points exactly once
     public LineSegment[] segments() {
-        return null;
+        return (LineSegment[]) segments.toArray();
     }
 
-    private static void recursiveCombinations(Object[] combo, int cdx, int ndx, Object[] elems) {
-        if(ndx == elems.length && cdx == combo.length)
-            System.out.println("** "+ Arrays.toString(combo));
+    private boolean isCollinear(Point[] points) {
+        double slope = points[0].slopeTo(points[1]);
+        for(int i=1; i<points.length-1; i++) {
+            if(points[i].slopeTo(points[i+1]) != slope)
+                return false;
+        }
+        return true;
+    }
+
+    private void findSegments(Point[] combo, int cdx, int ndx, Point[] elems) {
+        if(ndx == elems.length && cdx == combo.length && isCollinear(combo)) {
+            LineSegment seg = new LineSegment(combo[0], combo[cdx-1]);
+            segments.add(seg);
+        }
 
         if(ndx < elems.length) {
             if(cdx < combo.length) {
                 combo[cdx] = elems[ndx];
-                recursiveCombinations(combo, cdx + 1, ndx + 1, elems);
+                if(cdx>0 && combo[cdx-1].compareTo(combo[cdx])>0)
+                    findSegments(combo, cdx + 1, ndx + 1, elems);
             }
 
-            recursiveCombinations(combo, cdx, ndx + 1, elems);
+            findSegments(combo, cdx, ndx + 1, elems);
         }
     }
 
-    public static void recursiveCombinations(Integer[] points, int r) {
-        Object[] combination = new Object[r];
-        recursiveCombinations(combination, 0, 0, points);
+    private void findSegments(Point[] points, int r) {
+        Point[] combination = new Point[r];
+        findSegments(combination, 0, 0, points);
     }
 }
--- collinear/src/test/java/TestBruteCollinearPoints.java
+++ collinear/src/test/java/TestBruteCollinearPoints.java
@@ -9,8 +9,9 @@ import junit.framework.TestCase;
 public class TestBruteCollinearPoints extends TestCase {
 
     public void testrecursiveCombinations() {
-        Integer[] ints = {1,2,3,4,5,6};
-        BruteCollinearPoints.recursiveCombinations(ints, 4);
+//        Integer[] ints = {1,2,3,4,5,6};
+//        BruteCollinearPoints brute = new BruteCollinearPoints();
+//        brute.recursiveCombinations(ints, 4);
     }
 
 }
